fc1ad893b2dc2dbbf8783d18576e71f7d42a1869
viekaa <aryaadhitya158@gmail.com>
2025-09-01 11:26:23 +0700

    tambah about & edit index

--- about.php
+++ about.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Dasar</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php $nama = "Arya";
-          $umur = 16 ; ?>
-    <h1>Nama Saya, <?php echo " $nama "; ?> Umur Saya <?php echo " $umur ";?></h1>
+      Ini adalah halaman about
 </body>
 </html>
